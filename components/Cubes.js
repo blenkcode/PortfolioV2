bdbe333d0ed500f398ef672f3228ad0c4b31f1ae
Your name <valentinmor.pro@gmail.com>
2024-09-16 15:48:00 +0200

    debug

--- components/Cubes.js
+++ components/Cubes.js
@@ -1,29 +1,13 @@
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
 import Spline from "@splinetool/react-spline";
-const Spline = dynamic(() => import("@splinetool/react-spline"), {
-  ssr: false,
-});
 
-function Cubes({ onLoad }) {
-  const [loading, setLoading] = useState(true);
-
-  const handleLoad = (spline) => {
-    setLoading(false); // Masquer le loader une fois que la scène est chargée
-    if (typeof onLoad === "function") {
-      onLoad(spline);
-    }
-  };
-
+export default function Cubes() {
   return (
-    <div>
-      {/* Loader */}
+    <main>
       <Spline
-        onLoad={handleLoad}
         scene="https://prod.spline.design/NLRP-W7ilp5d5eP8/scene.splinecode"
+        width={844}
+        height={597}
       />
-    </div>
+    </main>
   );
 }
-
-export default Cubes;

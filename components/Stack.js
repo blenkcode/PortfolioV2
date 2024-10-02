4b76be331482b1173a080d8ca41d73e955d0c06c
Your name <valentinmor.pro@gmail.com>
2024-10-02 17:08:28 +0200

    res

--- components/Stack.js
+++ components/Stack.js
@@ -79,8 +79,8 @@ function Stack({}) {
           <div className="2xl:px-16 sm:px-7  lg:w-1/3  lg:translate-x-7 rounded-lg z-10  lg:mt-0 mt-10 relative ">
             <p className="2xl:text-lg  xl:text-base lg:text-sm font-thin text-md text-zinc-200 z-20  font-Satoshi  ">
               J'utilise <b className="text-violet-400 font-bold">TypeScript</b>{" "}
-              pour vous offrir des sites modernes performants et aisemment
-              maintenable. <br /> <br />
+              pour vous offrir des sites modernes, performants et aisément
+              maintenables <br /> <br />
               Pour le développement front-end, j'emploie les frameworks
               <b className="text-violet-400 font-bold"> React</b> et sa
               surcouche <b className="text-violet-400 font-bold">Next.js</b>.
@@ -90,7 +90,7 @@ function Stack({}) {
               <b className="text-violet-400 font-bold">Tailwind CSS</b> &{" "}
               <b className="text-violet-400 font-bold">Gsap</b>
               <br /> <br />
-              Pour la gestion de vos serveurs et data-base je m'appuie sur{" "}
+              Pour la gestion de vos serveurs et data-bases je m'appuie sur{" "}
               <b className="text-violet-400 font-bold">Node.js</b>,{" "}
               <b className="text-violet-400 font-bold">Express</b> et{" "}
               <b className="text-violet-400 font-bold">MongoDB</b>
